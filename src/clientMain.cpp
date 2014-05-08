cf28854ff2ee564f1d67718debe7ddaf3fc083a4
Soong <sos1991@qq.com>
2014-05-08 21:00:21 +0800

    add MainServer and MainClient

---
+++ src/clientMain.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: clientMain.cpp
+	> Author: Soong
+	> Mail:sos1991@qq.com 
+	> Created Time: 2014年05月07日 星期三 15时29分05秒
+ ************************************************************************/
+
+#include "UDPclient.h"
+
+int main(int argc, char **argv)
+{
+	UDPclient client(argv[1], argv[2]);
+	client.start();
+	return 0;
+}
---
+++ src/serverMain.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: serverMain.cpp
+	> Author: Soong
+	> Mail:sos1991@qq.com 
+	> Created Time: 2014年05月06日 星期二 21时51分01秒
+ ************************************************************************/
+
+#include "ThreadPool.h"
+#include "UDPserver.h"
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+	ThreadPool pool(10);	 //初始化线程池
+	pool.start_thread_pool();		//启动线程池
+	UDPserver server(argv[1], argv[2], &pool);		//初始化UDP服务器
+	server.start();		//启动UDP服务器
+}
